2ff177f4c563b35ac6e5f0a70180c90a2774ba25
Jui-Shan Liang <jsliang.tw@gmail.com>
2016-08-18 23:23:14 +0800

    install superagent

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+import request from 'superagent';
 
-document.addEventListener('DOMContentLoaded', function () {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    document.getElementById('app').innerHTML = tabs[0].url;
+const getRapidViewId = url => {
+  const re = /rapidView=(\d+)/;
+  const m = re.exec(url);
+  if (m) {
+    return m[1];
+  }
+  return null;
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.tabs.query({ currentWindow: true, active: true }, tabs => {
+    const rapidViewId = getRapidViewId(tabs[0].url);
+    request
+      .get(`https://appier.atlassian.net/rest/greenhopper/1.0/xboard/work/allData.json?rapidViewId=${rapidViewId}`)
+      .withCredentials()
+      .then(res => {
+        document.getElementById('app').innerHTML = res.body;
+      });
   });
 });
