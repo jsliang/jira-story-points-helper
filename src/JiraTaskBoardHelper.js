5e7b29413e36e1599235b8d863c874040c6d961f
Jui-Shan Liang <jsliang.tw@gmail.com>
2016-08-19 00:18:32 +0800

    aggregateIssuesByAssignee()

--- src/JiraTaskBoardHelper.js
+++ src/JiraTaskBoardHelper.js
@@ -1,5 +1,32 @@
 import $ from 'jquery';
 import request from 'superagent';
+import { fromJS, Map } from 'immutable';
+
+const aggregateIssuesByAssignee = issues => {
+  const assignees = issues.reduce((reduction, issue) => {
+    const assigneeId = issue.get('assignee');
+    if (reduction.has(assigneeId)) {
+      return reduction;
+    }
+
+    return reduction.set(assigneeId, Map({
+      name: issue.get('assigneeName'),
+      avatarUrl: issue.get('avatarUrl'),
+    }));
+  }, Map());
+
+  const pointsByAssignee = issues.reduce((reduction, issue) => {
+    const assigneeId = issue.get('assignee');
+    return reduction.update(assigneeId, (status = Map()) => {
+      const statusCategory = issue.getIn(['status', 'statusCategory', 'key']);
+      return status.update(statusCategory, (d = 0) =>
+        d + (issue.getIn(['estimateStatistic', 'statFieldValue', 'value']) || 0)
+      );
+    });
+  }, Map());
+
+  return { assignees, pointsByAssignee };
+};
 
 class JiraTaskBoardHelper {
   constructor() {
@@ -27,13 +54,19 @@ class JiraTaskBoardHelper {
       .get(`https://appier.atlassian.net/rest/greenhopper/1.0/xboard/work/allData.json?rapidViewId=${rapidViewId}`)
       .withCredentials()
       .then(res => {
-        this.allData = res.body;
+        this.issues = fromJS(res.body.issuesData.issues);
+
+        const { assignees, pointsByAssignee } = aggregateIssuesByAssignee(this.issues);
+        this.assignees = assignees;
+        this.pointsByAssignee = pointsByAssignee;
+
         this.updateView();
       });
   }
 
   updateView() {
-    console.log(this.allData);
+    console.log(this.assignees.toJS());
+    console.log(this.pointsByAssignee.toJS());
   }
 }
 
