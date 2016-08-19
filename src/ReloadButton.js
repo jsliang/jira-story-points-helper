3f6312f85f65d64efd59b9f4666c027e602145f3
Jui-Shan Liang <jsliang.tw@gmail.com>
2016-08-19 11:11:46 +0800

    use reload icon

--- src/ReloadButton.js
+++ src/ReloadButton.js
@@ -57,7 +57,14 @@ class ReloadButton extends PureComponent {
         }}
         title="Reload data"
       >
-        { showReloadIcon ? 'R' : 'J' }
+        { showReloadIcon ? (
+          <svg width="14" height="14" viewBox="0 0 16 16">
+            <path
+              d="M13.901 2.599A8 8 0 0 0 0 8h1.5a6.5 6.5 0 0 1 11.339-4.339L10.5 6H16V.5l-2.099 2.099zM14.5 8a6.5 6.5 0 0 1-11.339 4.339L5.5 10H0v5.5l2.099-2.099A8 8 0 0 0 16 8h-1.5z"
+              fill="#ffffff"
+            />
+          </svg>
+        ) : 'J' }
       </div>
     );
   }
