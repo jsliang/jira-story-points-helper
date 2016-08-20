b078c4b0897364babb0dbd5c2f9c3b8ddca9dfa6
Jui-Shan Liang <jsliang.tw@gmail.com>
2016-08-20 22:54:02 +0800

    refactor IconReload

--- src/components/ReloadButton.js
+++ src/components/ReloadButton.js
@@ -1,6 +1,8 @@
 import PureComponent from 'react-pure-render/component';
 import React from 'react';
 
+import IconReload from '../icons/IconReload';
+
 class ReloadButton extends PureComponent {
   constructor() {
     super();
@@ -58,14 +60,7 @@ class ReloadButton extends PureComponent {
         }}
         title={chrome.i18n.getMessage('txtReloadData')}
       >
-        { showReloadIcon ? (
-          <svg width="14" height="14" viewBox="0 0 16 16">
-            <path
-              d="M13.901 2.599A8 8 0 0 0 0 8h1.5a6.5 6.5 0 0 1 11.339-4.339L10.5 6H16V.5l-2.099 2.099zM14.5 8a6.5 6.5 0 0 1-11.339 4.339L5.5 10H0v5.5l2.099-2.099A8 8 0 0 0 16 8h-1.5z"
-              fill="#ffffff"
-            />
-          </svg>
-        ) : 'J' }
+        { showReloadIcon ? <IconReload color='#ffffff' /> : 'J' }
       </div>
     );
   }
---
+++ src/icons/IconReload.js
@@ -0,0 +1,23 @@
+import PureComponent from 'react-pure-render/component';
+import React from 'react';
+
+class IconReload extends PureComponent {
+  render() {
+    const { color, height, width } = this.props;
+    return (
+      <svg width={width} height={height} viewBox="0 0 16 16">
+        <path
+          d="M13.901 2.599A8 8 0 0 0 0 8h1.5a6.5 6.5 0 0 1 11.339-4.339L10.5 6H16V.5l-2.099 2.099zM14.5 8a6.5 6.5 0 0 1-11.339 4.339L5.5 10H0v5.5l2.099-2.099A8 8 0 0 0 16 8h-1.5z"
+          fill={color}
+        />
+      </svg>
+    );
+  }
+}
+
+IconReload.defaultProps = {
+  height: 14,
+  width: 14,
+};
+
+export default IconReload;
