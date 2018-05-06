79f98c4579b58294f49d2320726f3ea9452d1726
Jui-Shan Liang <jsliang.tw@gmail.com>
2018-05-06 20:54:24 +0800

    refactor SprintToggle

--- src/components/SprintToggle.js
+++ src/components/SprintToggle.js
@@ -1,5 +1,44 @@
 import PureComponent from 'react-pure-render/component';
 import React from 'react';
+import styled from 'styled-components';
+
+class ToggleButton extends PureComponent {
+  static defaultProps = {
+    expanded: false,
+    hover: false,
+    name: '',
+    onMouseEnter: () => {},
+    onMouseLeave: () => {},
+  };
+
+  render() {
+    const {
+      className,
+      expanded,
+      hover,
+      name,
+      onMouseEnter,
+      onMouseLeave,
+    } = this.props;
+
+    return (
+      <div
+        className={className}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
+        {expanded ? '+' : '-'} {name}
+      </div>
+    );
+  }
+}
+
+const StyledToggleButton = styled(ToggleButton)`
+  ${({ expanded }) => (expanded ? '' : 'color: #707070')};
+  cursor: pointer;
+  font-weight: bold;
+  text-decoration: ${({ hover }) => (hover ? 'underline' : 'none')};
+`;
 
 export default class SprintToggle extends PureComponent {
   static defaultProps = {
@@ -9,13 +48,13 @@ export default class SprintToggle extends PureComponent {
 
   constructor(props) {
     super(props);
-
     this.state = { hover: false };
   }
 
   onMouseEnter = () => {
     this.setState({ hover: true });
   };
+
   onMouseLeave = () => {
     this.setState({ hover: false });
   };
@@ -27,18 +66,13 @@ export default class SprintToggle extends PureComponent {
     } = this;
 
     return (
-      <div
+      <StyledToggleButton
+        expanded={expanded}
+        hover={hover}
+        name={name}
         onMouseEnter={this.onMouseEnter}
         onMouseLeave={this.onMouseLeave}
-        style={{
-          color: expanded ? undefined : '#707070',
-          cursor: 'pointer',
-          fontWeight: 'bold',
-          textDecoration: hover ? 'underline' : 'none',
-        }}
-      >
-        {expanded ? '+' : '-'} {name}
-      </div>
+      />
     );
   }
 }
