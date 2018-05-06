a71097fbc4687d3b75b2b3c0e147085760fb20d8
Jui-Shan Liang <jsliang.tw@gmail.com>
2018-05-06 23:52:09 +0800

    add unit test for getStatusCategoryMap

--- src/fetchData.test.js
+++ src/fetchData.test.js
@@ -1,4 +1,9 @@
-import { getAccountId, getRapidViewId, addPointsByCategory } from './fetchData';
+import {
+  getAccountId,
+  getRapidViewId,
+  addPointsByCategory,
+  getStatusCategoryMap,
+} from './fetchData';
 
 test('getAccountId: "test.atlassian.net" => "test"', () => {
   expect(getAccountId('test.atlassian.net')).toBe('test');
@@ -41,5 +46,88 @@ test('addPointsByCategory #4', () => {
   expect(addPointsByCategory({ new: 2.6, done: 0.5 }, 'done', 0.3)).toEqual({
     new: 2.6,
     done: 0.8,
+  });
+});
+
+test('getStatusCategoryMap', () => {
+  expect(
+    getStatusCategoryMap([
+      {
+        id: 104,
+        mappedStatuses: [
+          {
+            id: '10200',
+            statusCategory: { key: 'new' },
+          },
+          {
+            id: '11004',
+            statusCategory: { key: 'new' },
+          },
+          {
+            id: '10600',
+            statusCategory: { key: 'indeterminate' },
+          },
+          {
+            id: '11005',
+            statusCategory: { key: 'new' },
+          },
+        ],
+      },
+      {
+        id: 105,
+        mappedStatuses: [
+          {
+            id: '10401',
+            statusCategory: { key: 'indeterminate' },
+          },
+          {
+            id: '10300',
+            statusCategory: { key: 'indeterminate' },
+          },
+        ],
+      },
+      {
+        id: 134,
+        mappedStatuses: [
+          {
+            id: '11704',
+            statusCategory: { key: 'indeterminate' },
+          },
+        ],
+      },
+      {
+        id: 109,
+        mappedStatuses: [
+          {
+            id: '10501',
+            statusCategory: { key: 'done' },
+          },
+        ],
+      },
+      {
+        id: 111,
+        mappedStatuses: [
+          {
+            id: '10201',
+            statusCategory: { key: 'done' },
+          },
+          {
+            id: '5',
+            statusCategory: { key: 'done' },
+          },
+        ],
+      },
+    ])
+  ).toEqual({
+    '5': 'done',
+    '10200': 'new',
+    '10201': 'done',
+    '10300': 'indeterminate',
+    '10401': 'indeterminate',
+    '10501': 'done',
+    '10600': 'indeterminate',
+    '11004': 'new',
+    '11005': 'new',
+    '11704': 'indeterminate',
   });
 });
