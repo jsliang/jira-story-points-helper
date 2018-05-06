9a46d760a514b50e9a5625e0601780ed3be21019
Jui-Shan Liang <jsliang.tw@gmail.com>
2018-05-06 21:29:32 +0800

    refactor SprintTable

---
+++ src/components/AssigneeRow.js
@@ -0,0 +1,50 @@
+import PureComponent from 'react-pure-render/component';
+import React from 'react';
+import styled from 'styled-components';
+
+import { formatNumber } from './util';
+
+import PointsBar from './PointsBar';
+
+class AssigneeRow extends PureComponent {
+  static defaultProps = {
+    avatarUrl: '',
+    name: '',
+    points: 0,
+    totalPoints: 0,
+  };
+
+  render() {
+    const { avatarUrl, className, name, points, totalPoints } = this.props;
+
+    return (
+      <tr className={className}>
+        <td>
+          <img alt={name} className="ghx-avatar-img" src={avatarUrl} />
+          &nbsp;
+          <span>{name}</span>
+        </td>
+        <td className="total">
+          <div className="total-points">{formatNumber(totalPoints)}</div>
+        </td>
+        <td>
+          <PointsBar points={points} />
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default styled(AssigneeRow)`
+  .total {
+    padding: 0 6px;
+  }
+
+  .total-points {
+    align-items: center;
+    display: flex;
+    height: 100%;
+    justify-content: flex-end;
+    width: 100%;
+  }
+`;
